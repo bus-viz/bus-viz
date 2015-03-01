71c7383aff0e98659722a90f79ab8f0e1404ed47
Taylor Denouden <taylordenouden@gmail.com>
2015-03-01 13:19:57 -0800

    Create blank leaflet map with mapnik tile layers
    Add bw and color tilelayers to map and initialize the map on the map div. One of these layers can be removed when we decide which looks better.

---
+++ assets/js/map.js
@@ -0,0 +1,34 @@
+// GLOBAL VARIABLES
+var map;
+
+// Function to create a blank leaflet map on the #map element
+function initMap(){
+  // Init basemaps
+  var mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: "<a href='https://openstreetmap.org/'>&copy OpenStreetMap contributors, CC-BY-SA</a>"
+  });
+  var mapnik_bw = L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
+    attribution: "<a href='https://openstreetmap.org/'>&copy OpenStreetMap contributors, CC-BY-SA</a>"
+  });
+
+  var baseMaps = {
+    "Grayscale": mapnik_bw,
+    "Color": mapnik
+  };
+
+  // Init overlays
+
+  var overlayMaps = {
+      //eg "Bus stops": busstops
+  };
+
+  // Init map
+  map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13,
+    layers: [mapnik_bw]
+  });
+
+  // Add layers to map
+  L.control.layers(baseMaps, overlayMaps).addTo(map);
+};
