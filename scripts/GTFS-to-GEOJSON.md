9a19d9ccb1d20aa28220a10a6b70df977d0cc05f
Taylor Denouden <taylordenouden@gmail.com>
2015-03-13 10:59:40 -0700

    Create stops and routes json files

---
+++ scripts/GTFS-to-GEOJSON.md
@@ -0,0 +1,1 @@
+Ruby script to convert `stops.txt` and `shapes.txt` to GeoJSON.
---
+++ scripts/shapes.rb
@@ -0,0 +1,33 @@
+require 'csv'
+require 'json'
+
+# TODO: scripts expects shape_id and shape_pt_sequence to be in order in file
+# e.g. all points of single shape together, and shape_pt_sequence increasing
+
+features = []
+
+coordinates = []
+shape_id = nil 
+CSV.foreach('shapes.txt', headers: true) do |row|  
+  if row['shape_id'] != shape_id
+    # new shape!    
+    if shape_id
+      features << {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: coordinates
+        }
+      }
+    end    
+    coordinates = []
+    shape_id = row['shape_id']
+  end
+
+  coordinates << [row['shape_pt_lon'].to_f, row['shape_pt_lat'].to_f]
+end
+
+File.open('shapes.json', 'w') do |f|
+  f.write(JSON.pretty_generate({type: 'FeatureCollection', features: features}))
+end
