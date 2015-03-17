9e588e0f3d63be3d03030a590ab2b8d46635b790
Taylor Denouden <taylordenouden@gmail.com>
2015-03-17 14:42:51 -0700

    Clean up script

--- scripts/GTFS-to-GEOJSON.md
+++
@@ -1,1 +0,0 @@
-Ruby script to convert `stops.txt` and `shapes.txt` to GeoJSON.
--- scripts/shapes.py
+++ scripts/shapes.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
+# Written by Taylor Denouden March 17, 2014
+
 # Script to convert GTFS shapes to geojson
+# Assumes GTFS file has geometry segments sorted correctly by shape_id
+
 import sys
 import csv
 import json
 
 def main(argv):
     if len(argv) < 1:
-        print "Usage: ./shapes.py outputFile.json [inputFile.txt]"
+        print "Usage: ./shapes.py outputFile.js [path/to/shapes.txt]"
 
+    csvFile = open(argv[1] if argv[1] else "shapes.txt")
     features = []
     shape_id = None
 
-    csvFile = open(argv[1])
-    reader = csv.DictReader(csvFile)
-
-    for row in reader:
+    for row in csv.DictReader(csvFile):
         if row['shape_id'] != shape_id:
             # new shape!
             feature = {
@@ -29,14 +31,13 @@ def main(argv):
             }
             features.append(feature)
         else:
-            print features[-1].keys()
             features[-1]['geometry']['coordinates'].append([float(row['shape_pt_lon']), float(row['shape_pt_lat'])])
 
         shape_id = row['shape_id']
 
+
     with open(argv[0], 'w') as outfile:
         outfile.write("var bus_shapes = " + json.dumps({'type': 'FeatureCollection', 'features': features}))
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
