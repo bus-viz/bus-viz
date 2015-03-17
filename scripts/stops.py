66df4366bb9d0b1c4db20fe89072511c9193dd92
Taylor Denouden <taylordenouden@gmail.com>
2015-03-17 14:53:23 -0700

    Create conversion script for bus stops from GTFS to geojson

---
+++ scripts/stops.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import json
+
+def main(argv):
+    if len(argv) < 1:
+        print "Usage: ./stops.py outFile.js [/path/to/stops.txt]"
+
+    csvFile = open(argv[1] if argv[1] else "stops.txt")
+    features = []
+
+    for row in csv.DictReader(csvFile):
+        feature = {
+            'type': 'Feature',
+            'properties': {
+              'stop_id': row['stop_id'],
+              'stop_name': row['stop_name'],
+              'location_type': row['location_type'],
+              'parent_station': row['parent_station'],
+              'stop_short_name': row['stop_short_name']
+            },
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [float(row['stop_lon']), float(row['stop_lat'])]
+            }
+        }
+        features.append(feature)
+
+    with open(argv[0], 'w') as outfile:
+        outfile.write("var bus_stops = " + json.dumps({'type': 'FeatureCollection', 'features': features}))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
