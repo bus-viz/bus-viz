653287c9e85fc67290da396787e30b8f04e4f669
Taylor Denouden <taylordenouden@gmail.com>
2015-03-18 22:43:41 -0700

    More of integration because I didn't add them like a fool

--- src/js/map.js
+++ src/js/map.js
@@ -6,44 +6,21 @@ var mapnik_bw = L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{
   attribution: "<a href='https://openstreetmap.org/'>&copy OpenStreetMap contributors, CC-BY-SA</a>"
 });
 
-var baseMaps = {
-  "Grayscale": mapnik_bw,
-  "Color": mapnik
-};
-
-// Init overlays
-var overlayMaps = {
-    "Bus stops": L.geoJson(bus_stops, {
-      pointToLayer: function(feature, latlng){
-        return L.circleMarker(latlng,{
-          stroke: false,
-          opacity: 0.8,
-        }).setRadius(3);
-      }
-    }),
-    "Bus routes": L.geoJson(bus_shapes, {
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.shape_id);
-      }
-    })
-};
-
 // Init map
 var map = L.map('map', {
-  center: [52.50578645650081, 13.40080976486206],
+  // center: [52.50578645650081, 13.40080976486206], //Berlin
+  center: [48.44406299071947, -123.32316398620604], //Victoria
   zoom: 13,
   layers: [mapnik_bw]
 });
 
-// Add layers to map
-var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
 
 r360.config.serviceKey = 'CV397NJLTVWPVDEOO2MS';
 r360.config.serviceUrl = 'http://api.route360.net/api_dev/';
 var travelOptions = r360.travelOptions();
 var polygonLayer = r360.route360PolygonLayer();
 polygonLayer.addTo(map);
-layerControl.addOverlay(polygonLayer);
 
 // Create Route360 shapes
 map.on('dblclick', function(e){
