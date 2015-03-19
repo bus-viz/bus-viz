90f0a2d70aadf7d941d0ad6b2fb4bccf8eb61860
Taylor Denouden <taylordenouden@gmail.com>
2015-03-18 22:17:35 -0700

    Added route360 options and developer key and dblclick to add point

--- assets/js/map.js
+++ assets/js/map.js
@@ -30,10 +30,39 @@ var overlayMaps = {
 
 // Init map
 var map = L.map('map', {
-  center: [48.45368424076468, -123.34093093872069],
+  center: [52.50578645650081, 13.40080976486206],
   zoom: 12,
   layers: [mapnik_bw]
 });
 
 // Add layers to map
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+r360.config.serviceKey = 'CV397NJLTVWPVDEOO2MS';
+r360.config.serviceUrl = 'http://api.route360.net/api_dev/';
+var travelOptions = r360.travelOptions();
+var polygonLayer = r360.route360PolygonLayer();
+polygonLayer.addTo(map);
+layerControl.addOverlay(polygonLayer);
+
+// Create Route360 shapes
+map.on('dblclick', function(e){
+  var latlng = [e.latlng.lat, e.latlng.lng];
+  var marker = L.marker(latlng).addTo(map);
+
+  travelOptions.addSource(e.latlng);
+  // polygons for 5 to 30 minutes
+  travelOptions.setTravelTimes([300, 600, 900, 1200, 1500, 1800]);
+  travelOptions.setTravelType('car')
+
+  // call the service
+  r360.PolygonService.getTravelTimePolygons(travelOptions, function(polygons){
+    console.log(polygons);
+    polygonLayer.addLayer(polygons);
+    // map.fitBounds(polygonLayer.getBoundingBox());
+  },
+  function(status, message){
+      $("#r360-gettingstarted-error").show('fade').html("We are currently performing service maintenance. The \
+          service will be available shortly.");
+  });
+});
